13f1ad04213c6f4c1a01f42ea7d317f3d8c745b8
yakasov <yakasovian@gmail.com>
2025-03-21 23:03:21 +0000

    Fix some bad CSS and metal craft NaN

--- craft.js
+++ craft.js
@@ -3,13 +3,13 @@ function craft(recipe) {
     case "scraptometal":
       if (fish.scrap >= 5) {
         fish.scrap = fish.scrap - 5;
-        fish.metal = fish.metal + 1;
+        fish.metal = fish.metal ?? 0 + 1;
       }
       break;
     case "rusttometal":
       if (fish.rust >= 3) {
         fish.rust = fish.rust - 3;
-        fish.metal = fish.metal + 1;
+        fish.metal = fish.metal ?? 0 + 1;
       }
       break;
     case "metalfisher":
